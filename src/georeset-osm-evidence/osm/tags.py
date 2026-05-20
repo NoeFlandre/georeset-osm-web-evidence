86e577f1da75cd7b01b4882559ddec0021a27214
NoeFlandre <noeflandre@gmail.com>
2026-05-20 16:30:07 +0200

    Defined the OSM tags

---
+++ src/georeset-osm-evidence/osm/tags.py
@@ -0,0 +1,13 @@
+ENVIRONMENTAL_TAGS = [
+    ("landuse", "forest"),
+    ("landuse", "farmland"),
+    ("landuse", "meadow"),
+    ("landuse", "orchard"),
+    ("landuse", "vineyard"),
+    ("natural", "wood"),
+    ("natural", "wetland"),
+    ("natural", "grassland"),
+    ("natural", "heath"),
+    ("leisure", "natural reserve"),
+    ("boundary", "protected_area"),
+]
