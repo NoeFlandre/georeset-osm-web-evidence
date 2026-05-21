8c958ca090c6e818938ddc656762f6d17c97c9bb
NoeFlandre <noeflandre@gmail.com>
2026-05-21 09:59:01 +0200

    Start a tiny script and build package from src

---
+++ scripts/fetch_osm_polygons.py
@@ -0,0 +1,7 @@
+from georeset_osm_web_evidence.osm.overpass import (
+    build_polygon_query,
+    fetch_overpass_json,
+)
+from georeset_osm_web_evidence.osm.tags import ENVIRONMENTAL_TAGS
+
+print("hello")
