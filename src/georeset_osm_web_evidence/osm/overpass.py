0817ab7fface3dd02d1bb51491d68664d7550940
NoeFlandre <noeflandre@gmail.com>
2026-05-21 09:48:09 +0200

    Add the query overpass builder

--- src/georeset_osm_web_evidence/osm/overpass.py
+++ src/georeset_osm_web_evidence/osm/overpass.py
@@ -3,6 +3,28 @@ import requests
 OVERPASS_URL = "https://overpass-api.de/api/interpreter"
 
 
+def build_polygon_query(
+    south: float,
+    west: float,
+    north: float,
+    east: float,
+    tags: list[tuple[str, str]],
+) -> str:
+    tag_filters = "\n".join(
+        f' way["{key}"="{value}"]({south}, {west}, {north}, {east});\n'  # a way is a single closed polygon
+        f' relation["{key}"="{value}"]({south}, {west},{north},{east});'  # a relation is more complex polygon, often having holes or multiple parts
+        for key, value in tags
+    )
+    # we want the results back within 180s as a json of the gemoetry and the tags
+    return f"""
+[out:json][timeout:180];
+(
+{tag_filters}
+);
+out geom tags;
+    """
+
+
 def fetch_overpass_json(query: str) -> dict:
     response = requests.post(
         OVERPASS_URL,
