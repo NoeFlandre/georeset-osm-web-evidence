445a3c6187a3c7f21bc9af159b9370f991f10111
NoeFlandre <noeflandre@gmail.com>
2026-05-27 10:04:48 +0200

    Visualize the polygons with and without articles spatial distribution

--- scripts/visualize_osm_samples.py
+++ scripts/visualize_osm_samples.py
@@ -3,11 +3,26 @@ from georeset_osm_web_evidence.viz.map import create_polygon_map
 
 
 def main() -> None:
-    input_path = "data/processed/osm_polygons_sample_100.parquet"
-    output_path = "data/processed/osm_polygons_sample_100.html"
+    input_path = (
+        "data/processed/osm_polygons_sample100_wikipedia_balanced_50_50.parquet"
+    )
+    output_path = "data/processed/osm_polygons_sample100_wikipedia_balanced_50_50.html"
 
     gdf = load_geodataframe(input_path)
-    create_polygon_map(gdf, output_path)
+
+    map_gdf = gdf[
+        [
+            "osm_type",
+            "osm_id",
+            "area_km2",
+            "centroid_lon",
+            "centroid_lat",
+            "has_wikipedia_articles",
+            "geometry",
+        ]
+    ].copy()
+
+    create_polygon_map(map_gdf, output_path)
 
 
 if __name__ == "__main__":
